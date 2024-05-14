50554cf28e2673041a8eb506977eb8dd1b7d1685
RodriCordoba <rodricordoba119@gmail.com>
2024-05-14 09:25:29 -0300

    avance

--- Cordoba.Rodrigo.PrimerParcial/Cordoba.Rodrigo.PrimerParcial/FrmInicio.cs
+++ Cordoba.Rodrigo.PrimerParcial/Cordoba.Rodrigo.PrimerParcial/FrmInicio.cs
@@ -12,26 +12,16 @@ namespace Cordoba.Rodrigo.PrimerParcial
 {
     public partial class FrmInicio : Form
     {
-        private string nombreOperador;
         public FrmInicio(string nombreOperador)
         {
             InitializeComponent();
-            FrmInicio_Load(null, EventArgs.Empty);
-            this.nombreOperador = nombreOperador;
+            FrmInicio_Load();
             labelOperador.Text = "Operador: " + nombreOperador;
         }
-
-        private void FrmInicio_Load(object sender, EventArgs e)
+        private void FrmInicio_Load()
         {
-            string nombreOperador = "Nombre del Operador";
-            labelOperador.Text = "Operador: " + nombreOperador;
-
-            MessageBox.Show("Nombre del operador: " + nombreOperador);
-
             string fechaActual = DateTime.Today.ToString("dd/MM/yyyy");
             labelFecha.Text = "Fecha: " + fechaActual;
-
-            MessageBox.Show("Fecha actual: " + fechaActual);
         }
 
         private void statusStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
