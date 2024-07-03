d5d83e7b2f555d86bd5b8d923694f836ba8a8c25
RodriCordoba <rodricordoba119@gmail.com>
2024-07-03 03:37:15 -0300

    subiendo

--- Cordoba.Rodrigo.PrimerParcial/Indumentaria/Serializador.cs
+++ Cordoba.Rodrigo.PrimerParcial/Indumentaria/Serializador.cs
@@ -13,19 +13,14 @@ namespace Entidades.Indumentaria
     /// </summary>
     public class IndumentariaConvertidor : JsonConverter<Indumentaria>
     {
-        /// <summary>
-        /// Lee un objeto de tipo Indumentaria a partir de su representación JSON.
-        /// </summary>
-        /// <param name="reader">El lector de JSON.</param>
-        /// <param name="typeToConvert">El tipo de objeto que se va a convertir.</param>
-        /// <param name="options">Opciones de serialización JSON.</param>
-        /// <returns>El objeto de tipo Indumentaria.</returns>
         public override Indumentaria Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
             using (JsonDocument doc = JsonDocument.ParseValue(ref reader))
             {
                 JsonElement root = doc.RootElement;
                 string tipo = root.GetProperty("Tipo").GetString();
+                string codigo = root.GetProperty("Código").GetString();
+                int cantidad = root.GetProperty("Cantidad").GetInt32();
 
                 Indumentaria indumentaria = tipo switch
                 {
@@ -34,43 +29,41 @@ namespace Entidades.Indumentaria
                     "Pantalon" => JsonSerializer.Deserialize<Pantalon>(root.GetRawText(), options),
                     _ => throw new NotSupportedException($"Tipo {tipo} no soportado")
                 };
+
+                indumentaria.Codigo = codigo;
+                indumentaria.Cantidad = cantidad;
+
                 return indumentaria;
             }
         }
-        /// <summary>
-        /// Escribe un objeto de tipo Indumentaria en su representación JSON.
-        /// </summary>
-        /// <param name="writer">El escritor de JSON.</param>
-        /// <param name="value">El objeto de tipo Indumentaria a escribir.</param>
-        /// <param name="options">Opciones de serialización JSON.</param>
+
         public override void Write(Utf8JsonWriter writer, Indumentaria value, JsonSerializerOptions options)
         {
+            writer.WriteStartObject();
+
+            writer.WriteString("Tipo", value.GetType().Name);
+            writer.WriteString("Código", value.Codigo);
+            writer.WriteNumber("Cantidad", value.Cantidad);
+
             switch (value)
             {
                 case Campera campera:
-                    writer.WriteStartObject();
-                    writer.WriteString("Tipo", "Campera");
                     writer.WritePropertyName("Campera");
                     JsonSerializer.Serialize(writer, campera, options);
-                    writer.WriteEndObject();
                     break;
                 case Remera remera:
-                    writer.WriteStartObject();
-                    writer.WriteString("Tipo", "Remera");
                     writer.WritePropertyName("Remera");
                     JsonSerializer.Serialize(writer, remera, options);
-                    writer.WriteEndObject();
                     break;
                 case Pantalon pantalon:
-                    writer.WriteStartObject();
-                    writer.WriteString("Tipo", "Pantalon");
                     writer.WritePropertyName("Pantalon");
                     JsonSerializer.Serialize(writer, pantalon, options);
-                    writer.WriteEndObject();
                     break;
                 default:
                     throw new NotSupportedException($"Tipo {value.GetType()} no soportado");
             }
+
+            writer.WriteEndObject();
         }
     }
 }
